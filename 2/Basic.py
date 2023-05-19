8c606447a73b54edcfad6771c7217ae50a4db545
A1i98 <aliendprof@gmail.com>
2023-05-19 22:26:23 +0330

    2023.5.19

---
+++ 2/Basic.py
@@ -0,0 +1,48 @@
+# djfhj fgkjl
+
+# Variable = 5
+# Variable_name = 10
+# esmeKarbar = "ali"
+# seneKarbar = 20
+
+# esmeKarbar , seneKarbar = "ali" , 20
+
+
+# print(Variable)
+
+# x = 3
+# y = 10
+# # x = 20
+# # x = y
+# print(x)
+
+# z = x * y 
+# print(x * y)
+# print(x / y)
+# print(x - y)
+# print(x + y)
+# print(x // y)
+# print(x % y)
+# print(x ** y)
+
+# esmeKarbar = "ali"
+# esmeKarbar1 = "hossin"
+# space = " "
+
+# print(esmeKarbar +space+ esmeKarbar1)
+
+# محاسبه مساحت و حجم کره
+# pi = 3.14
+# radian = 5
+# area = 4 * pi * (radian ** 2)
+# print("area :",area)
+# volume = (4 / 3) * (pi * radian ** 3)
+# print(volume)
+
+# تبدیل سال به ثانیه
+# harsal = 31e6
+# seneAli = 22
+
+# print(harsal*seneAli)
+
+# تمرین برای هفته بعد حجم و مساحت استوانه
